5fcb69e1982b803bde49156f7aad06bcb4166071
Shrike6 <cody.stubbs@gmail.com>
2022-08-25 12:41:47 -0500

    got the prompter working

--- server.js
+++ server.js
@@ -12,58 +12,60 @@ const dbConfig = {
     database: "employees_db"
 };
 
-const connection = mysql.createConnection(dbConfig);
+const connection = mysql2.createConnection(dbConfig);
 
-db.query = util.promisify(db.query);
+connection.query = util.promisify(connection.query);
 
-const mainMenu = async () => {
-    try {
-      let response = await inquirer.prompt({
-        type: "list",
-        message: "What would you like to do?",
-        name: "choice",
-        choices: [
-          "View All Departments",
-          "View All Roles",
-          "View All Employees",
-          "Add Department",
-          "Add Role",
-          "Add Employee",
-          "Update Employee Info",
-        ],
-      });
-  
-      switch (response.choice) {
-        case "View All Departments":
-          viewAllDept();
-          break;
-  
-        case "View All Roles":
-          viewAllRole();
-          break;
-  
-        case "View All Employees":
-          viewAllEmpl();
-          break;
-  
-        case "Add Department":
-          addDept();
-          break;
-  
-        case "Add New Role":
-          addRole();
-          break;
-  
-        case "Add New Employee":
-          addEmpl();
-          break;
-  
-        case "Update Employee Info":
-          updEmpl();
-          break;
-      }
-    } catch (err) {
-      console.log(err);
-      mainMenu();
-    }
-  };
+const menu = [
+	{
+		type: "list",
+		message: "What would you like to do?",
+		name: "userResponse",
+		choices: [
+		    "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "Update Employee Info",
+		],
+	},
+];
+
+runApp = () => {
+    inquirer.prompt(menu)
+	.then((data) => {
+        switch (menu) {
+            case "View All Departments":
+                viewAllDept();
+                break;
+        
+            case "View All Roles":
+                viewAllRole();
+                break;
+        
+            case "View All Employees":
+                viewAllEmpl();
+                break;
+        
+            case "Add Department":
+                addDept();
+                break;
+        
+            case "Add New Role":
+                addRole();
+                break;
+        
+            case "Add New Employee":
+                addEmpl();
+                break;
+        
+            case "Update Employee Info":
+                updEmpl();
+                break;
+            } 
+    })    
+};
+
+runApp()
